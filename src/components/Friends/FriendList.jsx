4235efe1edb1f024eab8794d622d1ba9102282f9
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-06-08 20:11:39 +0300

    add-correct-proptypes-for-friend-list-component

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -12,10 +12,15 @@ const FriendList = ({ friends = [] }) => {
 };
 
 FriendList.propTypes = {
-  avatar: PropTypes.string,
-  name: PropTypes.string,
-  isOnline: PropTypes.bool,
-  id: PropTypes.number,
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+ 
 };
 
 export default FriendList;
